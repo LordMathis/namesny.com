7b0e3a65fcb42ca5b67e51f3beebdc8bb2ff15cc
LordMathis <namesny.matus@gmail.com>
2017-10-27 18:11:14 +0200

    Replace step with async

--- src/utils/compiler.js
+++ src/utils/compiler.js
@@ -3,18 +3,12 @@ const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
 const jsonfile = require('jsonfile');
-const Step = require('step');
+const async = require('async');
 const fm = require('front-matter');
 const config = require('../static/config/config.json');
 
 function readFile(filepath, callback) {
   fs.readFile(filepath, 'utf-8', callback);
-}
-
-function writeRenderedFile(renderedpath, result) {
-  fs.writeFile(renderedpath, result, (err) => {
-    if (err) throw err;
-  });
 }
 
 function render(file) {
@@ -36,42 +30,44 @@ function fileMetadata(filepath) {
   return metadata;
 }
 
+function compile(fileData, callback) {
+  const frontMatter = fm(fileData);
+  const rendered = render(frontMatter.body);
+  const metadata = fileMetadata(filepath);
+
+  const post = {
+    published: moment().format('MMMM Do YYYY'),
+    filename: metadata.filename,
+    title: frontMatter.attributes.title,
+    summary: frontMatter.attributes.summary,
+  };
+
+  const renderedpath = path.join(process.cwd(), config.renderPath, `${metadata.filename}.html`);
+
+  this.data.posts.push(post);
+
+  fs.writeFile(renderedpath, rendered, callback);
+}
+
 function Compiler(data) {
   this.data = data;
 }
 
 Compiler.prototype.addFile = function (filepath) {
-  Step(
-    function loadFiles() {
-      readFile(filepath, this.parallel());
-    },
-    (err, fileData) => {
-      if (err) throw err;
 
-      const frontMatter = fm(fileData);
-      const rendered = render(frontMatter.body);
-      const metadata = fileMetadata(filepath);
-
-      const post = {
-        published: moment().format('MMMM Do YYYY'),
-        filename: metadata.filename,
-        title: frontMatter.attributes.title,
-        summary: frontMatter.attributes.summary,
-      };
-
-      const renderedpath = path.join(process.cwd(), config.renderPath, `${metadata.filename}.html`);
-
-      this.data.posts.push(post);
-      writeRenderedFile(renderedpath, rendered);
-    }
-  );
-};
-
-Compiler.prototype.writeData = function () {
-  const dataPath = path.join(process.cwd(), 'src/utils/data.json');
-  jsonfile.writeFile(dataPath, this.data, (err) => {
+  async.waterfall([
+    readFile,
+    compile,
+  ], (err) => {
     if (err) throw err;
   });
+
+};
+
+Compiler.prototype.writeData = function (callback) {
+  const dataPath = path.join(process.cwd(), 'src/utils/data.json');
+  console.log(JSON.stringify(this.data));
+  jsonfile.writeFile(dataPath, this.data, callback);
 };
 
 module.exports = Compiler;
--- src/utils/scanner.js
+++ src/utils/scanner.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+const async = require('async');
 const Compiler = require('./compiler');
 const config = require('../static/config/config.json');
 const data = require('./data.json');
 
 module.exports = function() {
 
-  console.log(data);
   var compiler = new Compiler(data);
 
-  fs.readdir(config.contentPath, (err, files) => {
+  function readdir(callback) {
+    fs.readdir(config.contentPath, callback);
+  }
+
+  function compile(files, callback) {
     files.forEach(file => {
       const filePath = path.join(process.cwd(), config.contentPath, file);
       compiler.addFile(filePath);
     });
-    compiler.writeData();
-  });
+    return callback(null);
+  }
+
+  function writeData(callback) {
+    compiler.writeData(callback);
+  }
+
+  async.waterfall([
+    readdir,
+    compile,
+    writeData
+  ], (err => {
+    if(err) throw err;
+  }));
 }
