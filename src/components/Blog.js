a026c1f32192585cc6720ebe253145e34b0a5152
LordMathis <namesny.matus@gmail.com>
2017-11-07 17:09:45 +0100

    Blog post list styling

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,21 +9,35 @@ export default class Blog extends Component {
       );
     }
 
-    let posts = this.props.posts.map((post) => {
-      return (
-        <div>
-          <div>{post.published}</div>
-          <div>{post.filename}</div>
+    let posts = this.props.posts.map((post) =>
+      <div className="post-list-item" key={post.title}>
+        <div className="post-header">
+          <div className="post-title">
+            <h3><a href={post.link}>{post.title}</a></h3>
+          </div>
+          <div className="post-date">
+            <h3>{post.published}
+            </h3>
+          </div>
         </div>
-      )
-    })
+        <div className="post-summary">
+          <p>{post.summary}</p>
+        </div>
+        <div className="post-list-footer">
+          <a href={post.link}>Read More</a>
+        </div>
+
+      </div>
+    )
 
     return (
       <div>
         <div className="content">
           <h1>Blog</h1>
 
-          {posts}
+          <div className="blog-posts">
+            {posts}
+          </div>
 
         </div>
       </div>
