e5016c9fb1aad7304244983a0be1dd749d25101c
LordMathis <namesny.matus@gmail.com>
2017-11-07 18:17:44 +0100

    Create post api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,25 @@
 const data = require('./data.json');
 
 module.exports = function(app) {
-  app.get('/api/blog', ((req, res) => {
+  app.get('/api/blog', (req, res) => {
     res.json(data.posts);
-  }));
+  });
+
+  app.get('api/post/:postname', (req, res) => {
+    const post = data.posts.find((el) => {
+      el.filename === req.params.postname
+    });
+
+    if (post) {
+      res.json(post);
+    } else {
+      res.json({
+        error: 404
+      });
+    }
+  })
+}
+
+function (postname) {
+  data.posts
 }
