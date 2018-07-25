07c1e2c6c1a0ef1a40dc77a300243779bb8506f4
LordMathis <namesny.matus@gmail.com>
2018-07-25 21:30:45 +0200

    Add async and preload

--- src/utils/serverRender.js
+++ src/utils/serverRender.js
@@ -27,15 +27,15 @@ function renderFullPage(html, manifest) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Matúš Námešný</title>
           <!-- Google Fonts -->
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:700&amp;subset=latin-ext" rel="stylesheet">
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:700&amp;subset=latin-ext" rel="stylesheet" rel="preload">
           <!-- Font Awesome -->
-          <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+          <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" rel="preload" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
           <!-- Stylesheet -->
           ${process.env.NODE_ENV === 'production' ? `<link href=${manifest['bundle.css']} rel="stylesheet">` : ''}
       </head>
       <body>
         <div id="root">${process.env.NODE_ENV === 'production' ? html : `<div>${html}</div>`}</div>
-        <script src="${manifest['bundle.js']}"></script>
+        <script src="${manifest['bundle.js']}" async></script>
       </body>
     </html>
     `
