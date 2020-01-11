46143b832f1c551a2cb1e94b91b5f8aa64f4c222
LordMathis <matus@namesny.com>
2020-01-11 16:11:41 +0100

    Remove duplicate code

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -21,44 +21,33 @@ export default class Blog extends Component {
 
   render () {
     const md = MarkdownIt()
+    let postsHTML
 
     if (this.props.isLoading) {
-      return (
-        <div className={`${contentStyle.content}`} id="blog" role="region" aria-label="Blog posts">
-          <div className={styles.headerContainer}>
-            <Header header={'Blog'} role="heading" aria-level="2"/>
-            <SearchBox searchString={this.props.searchString}
-              expanded={this.props.expanded}
-              handleChange={this.props.handleChange}
-              handleFocus={this.props.handleFocus}
-              handleBlur={this.props.handleBlur}
-              handleSearch={this.props.handleSearch} />
+      postsHTML = <Spinner />
+    } else {
+      const posts = this.props.posts.sort((a, b) => {
+        return new Date(b.published) - new Date(a.published)
+      })
+
+      if (posts.length < 1) {
+        postsHTML = (
+          <div>
+            <span>No posts found</span>
           </div>
-          <Spinner />
-        </div>
-      )
-    }
-
-    const posts = this.props.posts.sort((a, b) => {
-      return new Date(b.published) - new Date(a.published)
-    })
-    let postsHTML = posts.map((post) =>
-      <div key={post.title} className={styles.postListItem} role="listitem">
-        <div className={styles.postHeader} >
-          <a href={post.link} className={styles.postTitle}>{post.title}</a>
-          <span className={styles.postDate}>{post.published}</span>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: md.render(post.summary) }}>
-        </div>
-      </div>
-    )
-
-    if (postsHTML.length < 1) {
-      postsHTML = (
-        <div>
-          <span>No posts found</span>
-        </div>
-      )
+        )
+      } else {
+        postsHTML = posts.map((post) =>
+          <div key={post.title} className={styles.postListItem} role="listitem">
+            <div className={styles.postHeader} >
+              <a href={post.link} className={styles.postTitle}>{post.title}</a>
+              <span className={styles.postDate}>{post.published}</span>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: md.render(post.summary) }}>
+            </div>
+          </div>
+        )
+      }
     }
 
     return (
