a4d28061c752e0fac3b793d83db06c4e2ded7d2e
LordMathis <matus@namesny.com>
2020-09-16 13:59:38 +0200

    Start over

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MainContainer } from './MainContainer'
-export { default as ContentContainer } from './ContentContainer'
-export { default as NotFoundContainer } from './NotFoundContainer'
-export { default as BlogContainer } from './BlogContainer'
